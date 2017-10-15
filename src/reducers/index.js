a21415e2218a0d0d86d2a803fb093f68849b4f2c
Gautam Gupta <gautam@gaut.am>
2017-10-15 14:56:56 -0400

    Remove example code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import { example } from './example';
+import { facebook } from './facebook';
 
 export default combineReducers({
-    example
+    facebook
 });
