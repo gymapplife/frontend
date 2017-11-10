33adfa4f9e7ca320286ef9245e7d6bb29d1c085e
Gautam Gupta <gautam@gaut.am>
2017-11-10 02:02:57 -0500

    Homepage design

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,22 @@ import {Card, CardTitle, CardText} from 'material-ui/Card'
 import RaisedButton from 'material-ui/RaisedButton'
 import * as constants from '../constants'
 import { Redirect } from 'react-router-dom'
+import './App.css'
 
 const Header = ({ loggedIn, signedUp, userInfo, handleFacebookResponse, handleLogout, handleRemoveAccount }) => {
     return (
-        <Card>
-            <CardTitle title="Welcome!" />
-            <CardText>
-                <FacebookLogin
-                    appId={constants.FACEBOOK_APP_ID}
-                    autoLoad={false}
-                    fields={constants.FACEBOOK_FIELDS}
-                    callback={handleFacebookResponse} />
-            </CardText>
+        <Card className="home-loggedout">
+            <div className="home-loggedout--align">
+                <CardTitle className="home-loggedout-text" title="GymApp.life" titleColor="white" titleStyle={{ fontSize: '58px', lineHeight: '72px', }}
+                    subtitle="Discover your fitness goals." subtitleColor="white" subtitleStyle={{ fontSize: '24px', fontStyle: 'italic', }} />
+                <CardText>
+                    <FacebookLogin
+                        appId={constants.FACEBOOK_APP_ID}
+                        autoLoad={false}
+                        fields={constants.FACEBOOK_FIELDS}
+                        callback={handleFacebookResponse} />
+                </CardText>
+            </div>
         </Card>
     )
 }
